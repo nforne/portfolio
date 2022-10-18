73bbc69a7f7896f14f298a35d28b86136cef03eb
nforne <mnforne@gmail.com>
2022-10-18 03:58:28 -0400

    reset build ready for test deploy

--- src/assets/social-icons/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  github: require(`./github.png`),
-  facebook: require(`./facebook.png`),
-  instagram: require(`./instagram.png`),
-  linkedin: require(`./linkedin.png`),
-  youtube: require(`./youtube.png`),
-};
